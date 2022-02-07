2c3d1c48cd8d097b39c81c3c2a0d3ccf0c537508
Pranav Satish Kumar <98470657+TheMegaCoderPro@users.noreply.github.com>
2022-02-07 18:28:28 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,12 @@
+function spanelem() {
+     window.alert("This is a SPAN Element");
+     document.geElementById("audio").play();
+    }
+    
+    function divelem() {
+     window.alert("This is a DIV Element");
+     document.geElementById("audio").play();
+    }
+    
+    
+    
